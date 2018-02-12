c89f53479fecb4b4a3cb4086d6c1e4e46952b081
Tianshu Huang <thetianshuhuang@gmail.com>
2018-02-12 15:48:06 -0600

    Experiments with trackers (many bugs on linux)

--- vision.py
+++ vision.py
@@ -4,21 +4,58 @@
 
 import cv2
 import pygame
+import numpy
+import time
+
+
+def get_output(camera):
+    frame = camera.read()[1]
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = numpy.rot90(frame)
+    return(frame)
+
 
 # initialize trackers
-fast_tracker = cv2.TrackerKCF_create()
-slow_tracker = cv2.TrackerMedianFlow_create()
+fast_tracker = cv2.Tracker_create("MIL")
+# slow_tracker = cv2.TrackerMedianFlow_create()
 
 camera = cv2.VideoCapture(0)
 
+frame = camera.read()[1]
+
+box = (250, 175, 100, 100)
+
 pygame.init()
-screen = pygame.display.set_mode(960, 540)
+screen = pygame.display.set_mode([600, 450])
+
+selected = False
+while(not selected):
+    frame = pygame.surfarray.make_surface(get_output(camera))
+    screen.blit(frame, (0, 0))
+
+    pygame.draw.rect(screen, (255, 0, 0), box, 1)
+    pygame.display.update()
+
+    if(pygame.key.get_pressed()[pygame.K_SPACE]):
+        selected = True
+
+    pygame.event.pump()
+
+fast_tracker.init(get_output(camera), box)
 
 while(True):
 
-    frame = camera.read()[1]
+    frame = get_output(camera)
 
     screen.fill([0, 0, 0])
+
+    box = fast_tracker.update(frame)[1]
+
+    # display screen
     frame = pygame.surfarray.make_surface(frame)
     screen.blit(frame, (0, 0))
+
+    pgbox = (box[1], box[0], box[3], box[2])
+    pygame.draw.rect(screen, (255, 0, 0), pgbox, 1)
+
     pygame.display.update()
